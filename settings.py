3bfd344809ce9a112bbf0b096b7cddfdbc67e81f
harry <harry@blackearthcollective.org.au>
2014-03-24 15:50:39 +0800

    re-installed blog app

--- settings.py
+++ settings.py
@@ -248,7 +248,7 @@ INSTALLED_APPS = (
     "mezzanine.conf",
     "mezzanine.core",
     "mezzanine.generic",
-    #"mezzanine.blog",
+    "mezzanine.blog",
     "mezzanine.forms",
     "mezzanine.pages",
     "mezzanine.galleries",
